a642951ba93b82209d655e607c7a253afca2fa16
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-06-21 13:34:28 +0530

    Create  insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO users
+(name, email, gender, date_of_birth)
+VALUES 
+('Debmalya Sau', 'saudebmalya52@gmail.com', 'male', '2005-06-19'),
+('Gouranga Mondal', 'mondalgouranga34@gmail.com', 'male', '2006-01-17');
+select * from users;
