1283b16c96a3e7572235b75584d34dc5f262febf
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-06-21 13:43:08 +0530

    Update basic_queries.sql

--- basic_queries.sql
+++ basic_queries.sql
@@ -2,13 +2,32 @@
 CREATE DATABASE startersql;
 
 -- step 2 (CREATING A TABLE)
-CREATE TABLLE USERS
+CREATE TABLE users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    gender ENUM('male', 'female', 'other'),
+    date_of_birth DATE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
+--  To drop the databse
+DROP DATABASE startersql;
 
-
-
+-- selecting data from table
 select * from users;
+
+-- add extra column using queries
 alter table users add column is_active boolean default true;
-select * from users;
+
+-- to delete a column of a table 
 alter table users drop column is_active ;
-alter table users modify column email varchar(120)
+
+-- To modify a table 
+alter table users modify column email varchar(120);
+
+-- To move a column after a column
+alter table users modify column email varchar(130) after id;
+
+-- TO move the column to first
+alter table users modify column date_of_birth DATETIME first;
