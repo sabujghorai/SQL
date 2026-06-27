7be9ca782ba473a80b6b572f5a2c118707698aed
SABUJ GHORAI <ghoraisabuj4@gmail.com>
2026-06-27 22:58:28 +0530

    Update create_tables.sql

--- create_tables.sql
+++ create_tables.sql
@@ -1,8 +1,12 @@
-CREATE TABLE users(
+CREATE DATABASE IF NOT EXISTS startersql;
+USE startersql;
+
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    gender ENUM('male', 'female', 'other'),
+    gender ENUM('Male', 'Female', 'Other'),
     date_of_birth DATE,
+    salary DECIMAL(10, 2),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
