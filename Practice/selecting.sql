c1fc27becee763d2f92402b2e7eb2e80b6edf4bf
sabujghorai <ghoraisabuj4@gmail.com>
2026-07-03 20:30:02 +0530

    updated the selecting.sql file

--- Practice/selecting.sql
+++ Practice/selecting.sql
@@ -52,3 +52,42 @@ select * from users where date_of_birth is not null;
 | 24 | Yash    | yash@example.com    | Male   | 1997-06-06    | 64000.00 | 2026-06-27 22:46:19 |
 +----+---------+---------------------+--------+---------------+----------+---------------------+
 
+
+-- Data finding using and queries 
+ select * from users where gender in ('female') and salary>'70000';
++----+----------+----------------------+--------+---------------+----------+---------------------+
+| id | name     | email                | gender | date_of_birth | salary   | created_at          |
++----+----------+----------------------+--------+---------------+----------+---------------------+
+|  2 | Ananya   | ananya@example.com   | Female | 1990-11-23    | 72000.00 | 2026-06-27 22:46:19 |
+|  6 | Priyanka | priyanka@example.com | Female | 1985-07-12    | 84000.00 | 2026-06-27 22:46:19 |
+|  9 | Meera    | meera@example.com    | Female | 1989-09-05    | 77000.00 | 2026-06-27 22:46:19 |
+| 15 | Nikita   | nikita@example.com   | Female | 1987-03-10    | 71000.00 | 2026-06-27 22:46:19 |
+| 23 | Lata     | lata@example.com     | Female | 1984-11-11    | 78000.00 | 2026-06-27 22:46:19 |
++----+----------+----------------------+--------+---------------+----------+---------------------+
+
+
+-- Data finding using or queries
+-- basically the or queri do that if the gender is male or the salary is greater than 70k then show the date 
+select * from users where gender in ('male') or salary > '70000';
+
++----+----------+----------------------+--------+---------------+----------+---------------------+
+| id | name     | email                | gender | date_of_birth | salary   | created_at          |
++----+----------+----------------------+--------+---------------+----------+---------------------+
+|  1 | Aarav    | aarav@example.com    | Male   | 1995-05-14    | 65000.00 | 2026-06-27 22:46:19 |
+|  2 | Ananya   | ananya@example.com   | Female | 1990-11-23    | 72000.00 | 2026-06-27 22:46:19 |
+|  3 | Raj      | raj@example.com      | Male   | 1988-02-17    | 58000.00 | 2026-06-27 22:46:19 |
+|  5 | Farhan   | farhan@example.com   | Male   | 1993-12-30    | 61000.00 | 2026-06-27 22:46:19 |
+|  6 | Priyanka | priyanka@example.com | Female | 1985-07-12    | 84000.00 | 2026-06-27 22:46:19 |
+|  8 | Aditya   | aditya@example.com   | Male   | 1992-06-17    | 69000.00 | 2026-06-27 22:46:19 |
+|  9 | Meera    | meera@example.com    | Female | 1989-09-05    | 77000.00 | 2026-06-27 22:46:19 |
+| 10 | Ishaan   | ishaan@example.com   | Male   | 2001-10-02    | 45000.00 | 2026-06-27 22:46:19 |
+| 12 | Rohan    | rohan@example.com    | Male   | 1986-12-01    | 75000.00 | 2026-06-27 22:46:19 |
+| 14 | Karan    | karan@example.com    | Male   | 1990-08-22    | 68000.00 | 2026-06-27 22:46:19 |
+| 15 | Nikita   | nikita@example.com   | Female | 1987-03-10    | 71000.00 | 2026-06-27 22:46:19 |
+| 16 | Manav    | manav@example.com    | Male   | 1996-11-29    | 61000.00 | 2026-06-27 22:46:19 |
+| 18 | Harshit  | harshit@example.com  | Male   | 1993-09-09    | 65000.00 | 2026-06-27 22:46:19 |
+| 20 | Imran    | imran@example.com    | Male   | 1995-07-30    | 63000.00 | 2026-06-27 22:46:19 |
+| 22 | Tushar   | tushar@example.com   | Male   | 1990-01-08    | 73000.00 | 2026-06-27 22:46:19 |
+| 23 | Lata     | lata@example.com     | Female | 1984-11-11    | 78000.00 | 2026-06-27 22:46:19 |
+| 24 | Yash     | yash@example.com     | Male   | 1997-06-06    | 64000.00 | 2026-06-27 22:46:19 |
++----+----------+----------------------+--------+---------------+----------+---------------------+
