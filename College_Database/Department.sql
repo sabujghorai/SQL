12b5cc9483569aac1c54b66fea89a8717bd07df1
sabujghorai <ghoraisabuj4@gmail.com>
2026-08-06 15:56:30 +0530

    updated the Department.sql file

--- College_Database/Department.sql
+++ College_Database/Department.sql
@@ -11,4 +11,23 @@ VALUES
 (109, 'Cyber Security', 'Kolkata'),
 (110, 'Human Resources', 'Gurugram');
 
+
+
 select * from Departments ;
++---------------+-------------------------------+-----------+
+| department_ID | Department_Name               | Location  |
++---------------+-------------------------------+-----------+
+|             1 | HR                            | Mumbai    |
+|             2 | IT                            | Kolkata   |
+|           101 | Computer Science              | Kolkata   |
+|           102 | Information Technology        | Bangalore |
+|           103 | Electronics and Communication | Hyderabad |
+|           104 | Mechanical Engineering        | Chennai   |
+|           105 | Civil Engineering             | Delhi     |
+|           106 | Electrical Engineering        | Mumbai    |
+|           107 | Artificial Intelligence       | Pune      |
+|           108 | Data Science                  | Noida     |
+|           109 | Cyber Security                | Kolkata   |
+|           110 | Human Resources               | Gurugram  |
++---------------+-------------------------------+-----------+
+12 rows in set (0.001 sec)
